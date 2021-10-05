6a5def51592b3cc104ddc10ae9e0e8c29bba8a50
hivecore <hivecore.io@gmail.com>
2021-10-05 17:18:07 +0600

    first deploy

--- src/auth/configuration.js
+++ src/auth/configuration.js
@@ -1,12 +1,12 @@
 const configuration = {
   client_id: '6vnP0utSSzj7NAp1eNp8glR4HYePMeHz2JDsbbuL',
-  redirect_uri: window.location.host+'/authentication/callback',
+  redirect_uri: window.location.origin+'/authentication/callback',
   response_type: 'code',
-  post_logout_redirect_uri: window.location.host+'/login',
+  post_logout_redirect_uri: window.location.origin+'/login',
   scope: 'openid bio profile',
   authority: 'https://0d83-103-218-24-121.ngrok.io',
   authority: 'https://sso.hivecorelimited.com',
-  silent_redirect_uri: window.location.host+'/authentication/silent_callback',
+  silent_redirect_uri: window.location.origin+'/authentication/silent_callback',
   automaticSilentRenew: true,
   loadUserInfo: true,
   revokeAccessTokenOnSignout:true
